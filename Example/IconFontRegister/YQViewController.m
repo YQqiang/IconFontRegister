dc06019a7d8b4f5665f0d4258274f9ca5ec16848
yuqiang <1054572107@qq.com>
2019-03-16 17:22:59 +0800

    gradient color

--- Example/IconFontRegister/YQViewController.m
+++ Example/IconFontRegister/YQViewController.m
@@ -20,7 +20,10 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    UIImage *image = [UIImage IFR_imageWithUnicode:@"\U0000eb74" fontName:@"YQIconFont" backgroundColor:UIColor.groupTableViewBackgroundColor iconColor:UIColor.redColor size:CGSizeMake(64, 64) imageInsets:UIEdgeInsetsMake(4, 4, 4, 4) imageOrientation:UIImageOrientationDownMirrored];
+//    UIImage *image = [UIImage IFR_imageWithUnicode:@"\U0000eb74" fontName:@"YQIconFont" backgroundColor:UIColor.groupTableViewBackgroundColor iconColor:UIColor.redColor size:CGSizeMake(64, 64) imageInsets:UIEdgeInsetsMake(4, 4, 4, 4) imageOrientation:UIImageOrientationDownMirrored];
+    
+    UIImage *image = [UIImage IFR_imageWithUnicode:@"\U0000eb74" fontName:@"YQIconFont" gradientColors:@[UIColor.blueColor, UIColor.yellowColor] gradientLocations:@[@(0.0), @(1.0)] gradientStartPoint:CGPointZero gradientEndPoint:CGPointMake(64, 64) iconColor:UIColor.redColor size:CGSizeMake(64, 64) imageInsets:UIEdgeInsetsZero imageOrientation:UIImageOrientationDownMirrored];
+    
     UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
     [self.view addSubview:imageView];
     imageView.center = self.view.center;
--- IconFontRegister/Classes/UIImage+IconFontRegister.m
+++ IconFontRegister/Classes/UIImage+IconFontRegister.m
@@ -16,9 +16,19 @@
                              size:(CGSize)size
                       imageInsets:(UIEdgeInsets)insets
                  imageOrientation:(UIImageOrientation)imageOrientation {
-    if (!bgColor) {
-        bgColor = [UIColor clearColor];
-    }
+    return [self IFR_imageWithUnicode:unicode fontName:fontName gradientColors:@[bgColor] gradientLocations:@[] gradientStartPoint:CGPointZero gradientEndPoint:CGPointMake(size.width, size.height) iconColor:iconColor size:size imageInsets:insets imageOrientation:imageOrientation];
+}
+
++ (UIImage *)IFR_imageWithUnicode:(NSString *)unicode
+                         fontName:(NSString *)fontName
+                   gradientColors:(NSArray <UIColor *>*)gradientColors
+                gradientLocations:(NSArray <NSNumber *>*)gradientLocations
+               gradientStartPoint:(CGPoint)gradientStartPoint
+                 gradientEndPoint:(CGPoint)gradientEndPoint
+                        iconColor:(UIColor *)iconColor
+                             size:(CGSize)size
+                      imageInsets:(UIEdgeInsets)insets
+                 imageOrientation:(UIImageOrientation)imageOrientation {
     if (!iconColor) {
         iconColor = [UIColor whiteColor];
     }
@@ -36,12 +46,37 @@
     
     UIGraphicsBeginImageContext(CGSizeMake(imageSizeW, imageSizeH));
     CGContextRef context = UIGraphicsGetCurrentContext();
-    if (bgColor) {
-        [bgColor set];
-        UIRectFill(CGRectMake(0.0, 0.0, imageSizeW, imageSizeH));
+    if (gradientColors.count > 0) {
+        // 绘制纯色背景
+        if (gradientColors.count == 1) {
+            [gradientColors.firstObject set];
+            UIRectFill(CGRectMake(0.0, 0.0, imageSizeW, imageSizeH));
+        }
+        // 绘制渐变背景
+        else {
+            CGColorSpaceRef deviceRGB = CGColorSpaceCreateDeviceRGB();
+            NSMutableArray *gradientCGColors = [NSMutableArray array];
+            for (UIColor *color in gradientColors) {
+                [gradientCGColors addObject:(id)color.CGColor];
+            }
+            
+            CGFloat locationsFloat[[gradientLocations count]];
+            for (int i = 0; i < [gradientLocations count]; i++) {
+                NSNumber *number = [gradientLocations objectAtIndex:i];
+                locationsFloat[i] = [number floatValue];
+            }
+            
+            CGPoint startPoint = CGPointMake(gradientStartPoint.x * scale, gradientStartPoint.y * scale);
+            CGPoint endPoint = CGPointMake(gradientEndPoint.x * scale, gradientEndPoint.y * scale);
+            
+            CGGradientRef gradient = CGGradientCreateWithColors(deviceRGB, (__bridge CFArrayRef)gradientCGColors, locationsFloat);
+            CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
+            CGColorSpaceRelease(deviceRGB);
+            CGGradientRelease(gradient);
+        }
     }
-    CGPoint point = CGPointMake(imageInsets.left * scale, imageInsets.top * scale);
     
+    CGPoint point = CGPointMake(imageInsets.left * scale, imageInsets.top * scale);
     if ([unicode respondsToSelector:@selector(drawAtPoint:withAttributes:)]) {
         [unicode drawAtPoint:point withAttributes:@{NSFontAttributeName:font, NSForegroundColorAttributeName: iconColor}];
     } else {
